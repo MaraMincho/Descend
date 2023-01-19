a792cbca061ee07c3c6d81d05afce5e713b328b1
ywe <42719337+Morrki@users.noreply.github.com>
2023-01-19 17:57:27 +0900

    Fix : Layout 배치 완료
    작업내용
     - CreateBoardScreen.dart
     - BoardDetail.dart
     - 위 두 파일 layout 작성완료
    
    Todo
     - Design 세부 조정
     - 하드코딩 값 정리하기
     - 주석 작성

--- lib/screens/CreateBoardScreen.dart
+++ lib/screens/CreateBoardScreen.dart
@@ -8,78 +8,137 @@ class CreateBoardScreen extends StatefulWidget {
   static const routename = '/CreateBoardScreen';
 
   @override
-  State<CreateBoardScreen> createState() => _SpecificScreenState();
+  State<CreateBoardScreen> createState() => _CreateBoardScreen();
 }
 
-class _SpecificScreenState extends State<CreateBoardScreen> {
+class _CreateBoardScreen extends State<CreateBoardScreen> {
   @override
   Widget build(BuildContext context) {
-    List <String> files = ["fil1.png", "file2.pdf", "files3.mp3"];
+    List<String> files = ["file1.png", "file2.pdf", "files3.mp3", "files4.sh"];
+
+    // Variables
+    int btnBg = 0xffD69595;
+    int btnTxtColor = 0xff000000;
+    double valPadding = 32.0;
 
     var size = MediaQuery.of(context).size;
     return Scaffold(
         body: SafeArea(
       child: ListView(
         children: <Widget>[
-          Column(
-            children: const [
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "현장프로젝트 교과",
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: valPadding),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Image.asset("images/icons/img.png")),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(btnBg),
+                    ),
+                    onPressed: () {
+                      Fluttertoast.showToast(
+                          msg: "Pressed : Submit",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.BOTTOM,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.grey,
+                          textColor: Colors.black,
+                          fontSize: 16.0);
+                    },
+                    child: Text(
+                      "완료",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Color(btnTxtColor),
+                      ),
+                    )),
+              ],
+            ),
+          ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: valPadding),
+              child: Column(
+                children: const [
+                  Text(
+                    "현장 프로젝트 교과",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "제목",
+                    ),
+                  ),
+                  TextField(
+                    maxLines: null,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "내용을 입력하세요",
+                      contentPadding: EdgeInsets.only(top: 10, bottom: 140),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 120),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Color(btnBg),
+              ),
+              onPressed: () {
+                Fluttertoast.showToast(
+                    msg: "Pressed : Add file",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.BOTTOM,
+                    timeInSecForIosWeb: 1,
+                    backgroundColor: Colors.grey,
+                    textColor: Colors.black,
+                    fontSize: 16.0);
+              },
+              child: Text(
+                "첨부파일 추가하기",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Color(btnTxtColor),
                 ),
               ),
-              TextField(
-                maxLines: null,
-                decoration: InputDecoration(
-                  hintText: "내용을 입력하세요",
-                  contentPadding: EdgeInsets.only(bottom: 100),
-                ),
-              ),
-            ],
+            ),
           ),
-          ElevatedButton(
-            onPressed: () {
-              Fluttertoast.showToast(
-                  msg: "Pressed : Add file button",
-                  toastLength: Toast.LENGTH_SHORT,
-                  gravity: ToastGravity.BOTTOM,
-                  timeInSecForIosWeb: 1,
-                  backgroundColor: Colors.grey,
-                  textColor: Colors.black,
-                  fontSize: 16.0);
-            },
-            child: Text("첨부파일 추가하기"),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 200,
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: valPadding),
             child: ListView.builder(
-                itemCount: files == null ? 0 : files.length,
+                shrinkWrap: true,
+                itemCount: files.length,
                 itemBuilder: (context, idx) {
                   return GestureDetector(
                     onTap: () {
-                      files.removeAt(idx);
+                      /// TODO 업데이트 안됨...
+                      setState(() {
+                        files.removeAt(idx);
+                      });
+                      Fluttertoast.showToast(
+                          msg: "Deleted file",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.BOTTOM,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.grey,
+                          textColor: Colors.black,
+                          fontSize: 16.0);
                     },
                     child: Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Container(
-                        child: Text("${files[idx]}"),
-                      ),
+                      padding: const EdgeInsets.all(10),
+                      child: Text("${files[idx]}"),
                     ),
                   );
                 }),
           ),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
-                border: Border.all(
-              color: Colors.black,
-              width: 1,
-            )),
-            child: Container(child: Image.asset('images/imgs/img.png')),
-          ),
         ],
-        // child: Image.asset("assets/images/glob_top_img_none.png",
       ),
     ));
   }
